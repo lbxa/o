e5a293964538ba595e499e4877bac53cb023e449
Lucas Barbosa <lucas.chu.barbosa@au.pwc.com>
2024-06-09 22:50:48 +1000

    looks like its here to stay

---
+++ packages/backend/src/types/graphql.ts
@@ -0,0 +1,54 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export interface CreateUserInput {
+    firstName: string;
+    lastName: string;
+    handle?: Nullable<string>;
+    email: string;
+    superQuickChanges?: Nullable<string>;
+    wellThatWasQuick?: Nullable<string>;
+}
+
+export interface UpdateUserInput {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+}
+
+export interface Post {
+    __typename?: 'Post';
+    id: number;
+    content: string;
+}
+
+export interface IQuery {
+    __typename?: 'IQuery';
+    health(): string | Promise<string>;
+    user(id: number): Nullable<User> | Promise<Nullable<User>>;
+}
+
+export interface User {
+    __typename?: 'User';
+    id?: Nullable<number>;
+    handle?: Nullable<string>;
+    firstName?: Nullable<string>;
+    lastName?: Nullable<string>;
+    email?: Nullable<string>;
+}
+
+export interface IMutation {
+    __typename?: 'IMutation';
+    createUser(createUserInput: CreateUserInput): User | Promise<User>;
+    updateUser(updateUserInput: UpdateUserInput): User | Promise<User>;
+}
+
+type Nullable<T> = T | null;
