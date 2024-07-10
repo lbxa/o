74d6453c1f234c0150b1604830084cd974649917
Lucas Barbosa <lucas.chu.barbosa@au.pwc.com>
2024-07-11 00:18:25 +1000

    Added Why us and Faq section

--- apps/home/src/universe/atoms/Card.tsx
+++
@@ -1,9 +0,0 @@
-import type { JSX, ParentProps } from "solid-js";
-
-export const Card: (props: ParentProps) => JSX.Element = (props) => {
-  return (
-    <div class="m-auto w-full bg-black text-white	opacity-80 backdrop-blur-3xl">
-      {props.children}
-    </div>
-  );
-};
--- apps/home/src/universe/atoms/index.ts
+++ apps/home/src/universe/atoms/index.ts
@@ -1,4 +1,3 @@
-export * from "./Card";
 export * from "./Li";
 export * from "./Title";
 export * from "./Ul";
