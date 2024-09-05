1f3fca128ae23ca589163e946231adb24749432b
Lucas Barbosa <lucas.chu.barbosa@au.pwc.com>
2024-09-05 19:53:59 +1000

    quick style fixes

--- apps/home/src/universe/molecules/Header.tsx
+++ apps/home/src/universe/molecules/Header.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
   // });
 
   return (
-    <div class="mx-auto mb-xl flex w-full flex-col justify-around rounded-b-3xl bg-black py-sm pl-sm text-center text-white lg:rounded-none lg:pl-0">
+    <div class="mx-auto mb-xl flex w-full flex-col justify-around rounded-b-3xl bg-black py-sm text-center text-white lg:rounded-none">
       <h1 class="inline bg-contain bg-clip-text bg-center text-6xl font-black text-white md:text-7xl">
         oNex
       </h1>
