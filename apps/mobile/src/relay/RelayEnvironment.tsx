726c40e694ced1c8ec42e795db24014e03c01807
Lucas Barbosa <lucas.chu.barbosa@au.pwc.com>
2024-09-27 22:28:51 +1000

    Auth typings wrapped for refreshTokens

--- apps/mobile/src/relay/RelayEnvironment.tsx
+++ apps/mobile/src/relay/RelayEnvironment.tsx
@@ -1,55 +1,14 @@
-import * as SecureStore from "expo-secure-store";
-import React, { useMemo } from "react";
+import type { PropsWithChildren } from "react";
+import React from "react";
 import { RelayEnvironmentProvider } from "react-relay";
-import type { FetchFunction, IEnvironment } from "relay-runtime";
-import { Environment, Network, RecordSource, Store } from "relay-runtime";
 
-const fetchFn: FetchFunction = async (request, variables) => {
-  const token = SecureStore.getItem("ACCESS_TOKEN");
-  console.log("CacheToke", token);
+import { useRelayEnvironment } from "./useRelayEnvironment";
 
-  const headers = {
-    Accept: "application/json",
-    "Content-type": "application/json",
-    ...(token ? { Authorization: `Bearer ${token}` } : {}),
-  };
-
-  const body = JSON.stringify({
-    query: request.text,
-    variables,
-  });
-
-  const response = await fetch("http://localhost:6969/graphql", {
-    method: "POST",
-    headers,
-    body,
-  });
-
-  const data = response.json();
-
-  if (response.status === 401) {
-    // Handle unauthorized error (e.g., token expired)
-    await SecureStore.deleteItemAsync("ACCESS_TOKEN");
-    // You might want to redirect to login screen or refresh token here
-  }
-
-  return data;
-};
-
-function createEnvironment(): IEnvironment {
-  const network = Network.create(fetchFn);
-  const store = new Store(new RecordSource());
-  return new Environment({ store, network });
-}
-
-export const RelayEnvironment = ({
+export const RelayEnvironment: React.FC<PropsWithChildren> = ({
   children,
-}: {
-  children: React.ReactNode;
 }): React.ReactElement => {
-  const environment = useMemo(() => {
-    return createEnvironment();
-  }, []);
+  const { createEnvironment } = useRelayEnvironment();
+  const environment = createEnvironment();
 
   return (
     <RelayEnvironmentProvider environment={environment}>
