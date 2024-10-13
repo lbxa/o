b37b7ff374028efc71aed9c1ba1c3e723774e23e
Lucas Barbosa <lucas.chu.barbosa@au.pwc.com>
2024-10-13 15:47:23 +1100

    Finally fixed all pnpm related issues with Rn stack

--- apps/mobile/metro.config.cjs
+++ apps/mobile/metro.config.cjs
@@ -1,93 +1,40 @@
 // Learn more: https://docs.expo.dev/guides/monorepos/
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { FileStore } = require("metro-cache");
 const { withNativeWind } = require("nativewind/metro");
 const { makeMetroConfig } = require("@rnx-kit/metro-config");
-const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
-
-// !This is my best shot!
 const { mergeConfig } = require("metro-config");
-
-const path = require("path");
+const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
 
 const symlinksResolver = MetroSymlinksResolver();
 
-// const config = (() => {
-//   const defaultConfig = getDefaultConfig(__dirname);
-//   const { transformer, resolver } = defaultConfig;
-
-//   const nativeWindConfig = withNativeWind(defaultConfig, {
-//     input: "./src/global.css",
-//     configPath: "./tailwind.config.ts",
-//   });
-
-//   return {
-//     ...nativeWindConfig,
-//     transformer: {
-//       ...nativeWindConfig.transformer,
-//       babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
-//     },
-//     resolver: {
-//       ...nativeWindConfig.resolver,
-//       assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-//       sourceExts: [...nativeWindConfig.resolver.sourceExts, "svg"],
-//     },
-//   };
-// })();
-
-/** @type {import('expo/metro-config').MetroConfig} */
-// const withSymlinkSupport = (config) => makeMetroConfig({
-//   ...config,
-//   resolver: {
-//     ...config.resolver,
-//     resolveRequest: (context, moduleName, platform) => {
-//       try {
-//         // Symlinks resolver throws when it can't find what we're looking for.
-//         const res = symlinksResolver(context, moduleName, platform);
-
-//         if (res) {
-//           return res;
-//         }
-//       } catch {
-//         // If we have an error, we pass it on to the next resolver in the chain,
-//         // which should be one of expos.
-//         // https://github.com/expo/expo/blob/9c025ce7c10b23546ca889f3905f4a46d65608a4/packages/%40expo/cli/src/start/server/metro/withMetroResolvers.ts#L47
-//         return context.resolveRequest(context, moduleName, platform);
-//       }
-//     },
-//   },
-// });
-
-/// !TEST
-
-// Striking a balance between Nativewind and rnx-kit was tricky
-// https://github.com/nativewind/nativewind/issues/926
+/**
+ * Striking a balance between Nativewind and rnx-kit was tricky
+ * @see https://github.com/nativewind/nativewind/issues/926
+ */
 
 const projectDir = __dirname;
 const monorepoRoot = path.resolve(projectDir, "../..");
-
-/** @type {import('expo/metro-config').MetroConfig} */
 const defaultConfig = getDefaultConfig(projectDir);
 
-const nativeWindConfig = withNativeWind(defaultConfig, {
-  input: path.join(projectDir, "./src/global.css"),
-  configPath: path.join(projectDir, "./tailwind.config.ts")
-});
-
 /** @type {import('expo/metro-config').MetroConfig} */
-module.exports = makeMetroConfig({
-  ...defaultConfig,
-  projectRoot: projectDir,
+const monorepoConfig = {
   resolver: {
-    ...defaultConfig.resolver,
-    ...nativeWindConfig.resolver,
     disableHierarchicalLookup: true,
-    assetExts: defaultConfig.resolver.assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...defaultConfig.resolver.sourceExts, "svg"],
     nodeModulesPaths: [
       path.resolve(projectDir, "node_modules"),
       path.resolve(monorepoRoot, "node_modules"),
     ],
+    /**
+     * React Native has very frail symlink support for modern monorepo tools  
+     * that rely on symlinks and global caches to dramatically increase the
+     * performance of installs e.g. pnpm. The best way around this is using
+     * Microsoft's rnx-kit. I've written more extensively about this in the
+     * README.
+     * 
+     * @see https://gist.github.com/Zn4rK/ed60c380e7b672e3089074f51792a2b8
+     */
     resolveRequest: (context, moduleName, platform) => {
       try {
         // Symlinks resolver throws when it can't find what we're looking for.
@@ -97,71 +44,64 @@ module.exports = makeMetroConfig({
           return res;
         }
       } catch {
-        // If we have an error, we pass it on to the next resolver in the chain,
-        // which should be one of expos.
-        // https://github.com/expo/expo/blob/9c025ce7c10b23546ca889f3905f4a46d65608a4/packages/%40expo/cli/src/start/server/metro/withMetroResolvers.ts#L47
+        /**
+         * If we have an error, we pass it on to the next resolver in the chain,
+         * which should be one of expos.
+         * @see https://github.com/expo/expo/blob/9c025ce7c10b23546ca889f3905f4a46d65608a4/packages/%40expo/cli/src/start/server/metro/withMetroResolvers.ts#L47
+         */
         return context.resolveRequest(context, moduleName, platform);
       }
     },
   },
-  transformer: {
-    ...defaultConfig.transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
-    // <3 -> https://github.com/kristerkari/react-native-svg-transformer/issues/141
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-    ...nativeWindConfig.transformer
-  },
-  transformerPath: nativeWindConfig.transformerPath,
+  /**
+   * Add the monorepo paths to the Metro config.
+   * This allows Metro to resolve modules from the monorepo.
+   *
+   * @see https://docs.expo.dev/guides/monorepos/#modify-the-metro-config
+   */
   watchFolders: [monorepoRoot],
+  /**
+   * Move the Metro cache to the `node_modules/.cache/metro` folder.
+   * This repository configured Turborepo to use this cache location as well.
+   * If you have any environment variables, you can configure Turborepo to invalidate it when needed.
+   * @see https://turbo.build/repo/docs/reference/configuration#env
+   */
   cacheStores: [
     new FileStore({
       root: path.join(projectDir, "node_modules", ".cache", "metro"),
-    }),
+    })
   ]
-});
+};
 
-/// !TEST
 
-/**
- * Add the monorepo paths to the Metro config.
- * This allows Metro to resolve modules from the monorepo.
- *
- * @see https://docs.expo.dev/guides/monorepos/#modify-the-metro-config
- * @param {import('expo/metro-config').MetroConfig} config
- * @returns {import('expo/metro-config').MetroConfig}
- */
-function withMonorepoPaths(config) {
-  const projectRoot = __dirname;
-  const monorepoRoot = path.resolve(projectRoot, "../..");
-
-  // #1 - Watch all files in the monorepo
-  config.watchFolders = [monorepoRoot];
-
-  // #2 - Resolve modules within the project's `node_modules` first, then all monorepo modules
-  config.resolver.nodeModulesPaths = [
-    path.resolve(projectRoot, "node_modules"),
-    path.resolve(monorepoRoot, "node_modules"),
-  ];
-
-  // config.resolver.unstable_enableSymlinks = true;
-
-  return config;
-}
+/** @type {import('expo/metro-config').MetroConfig} */
+const svgConfig = {
+  resolver: {
+    assetExts: defaultConfig.resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...defaultConfig.resolver.sourceExts, "svg"],
+  },
+  transformer: {
+    // <3 -> https://github.com/kristerkari/react-native-svg-transformer/issues/141
+    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+    babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+  },
+};
 
 /**
- * Move the Metro cache to the `node_modules/.cache/metro` folder.
- * This repository configured Turborepo to use this cache location as well.
- * If you have any environment variables, you can configure Turborepo to invalidate it when needed.
- *
- * @see https://turbo.build/repo/docs/reference/configuration#env
- * @param {import('expo/metro-config').MetroConfig} config
- * @returns {import('expo/metro-config').MetroConfig}
+ * Merging configs do not deeply merge arrays/functions. Keep this in mind to not
+ * override important properties. Order matters!
+ * 
+ * @see https://metrobundler.dev/docs/configuration/#merging-configurations
  */
-function withTurborepoManagedCache(config) {
-  config.cacheStores = [
-    new FileStore({ root: path.join(__dirname, "node_modules/.cache/metro") }),
-  ];
-  return config;
-}
+const finalConfig = makeMetroConfig(mergeConfig(defaultConfig, monorepoConfig, svgConfig));
 
-// module.exports = withTurborepoManagedCache(withMonorepoPaths(withSymlinkSupport(config)));
+/**
+ * Nativewind config must come last! Internally it uses withCssInterop to 
+ * resolve css imports. If this is overridden, Nativewind will not work.
+ * 
+ * @see https://github.com/nativewind/nativewind/issues/972#issuecomment-2329660147
+ */
+module.exports = withNativeWind(finalConfig, {
+  input: path.join(projectDir, "./src/global.css"),
+  configPath: path.join(projectDir, "./tailwind.config.ts")
+}); 
