101a8c40659525e719cc37a5410f96fa8293c8e2
Lucas Barbosa <lucas.chu.barbosa@au.pwc.com>
2024-11-11 13:00:11 +1100

    Prod bundling bug fixed

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -4,6 +4,7 @@ import "expo-dev-client";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { verifyInstallation } from "nativewind";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
@@ -12,6 +13,7 @@ import { ViewerProvider } from "@/users/Viewer";
 import { useToken } from "@/utils/useToken";
 
 export default function Root() {
+  verifyInstallation();
   const { token } = useToken();
 
   return (
