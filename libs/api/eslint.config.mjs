2a2c70150da689c2e870644d6c85a3fed5a17534
Lucas Barbosa <lucas.chu.barbosa@au.pwc.com>
2024-11-24 13:00:04 +1100

    Trying something on the pipelines

--- libs/api/eslint.config.mjs
+++ libs/api/eslint.config.mjs
@@ -2,10 +2,10 @@ import baseConfig from "@o/eslint/base"
 
 /** @type {import('typescript-eslint').Config} */
 export default [
+  ...baseConfig,
   {
     ignores: ["dist/**", "node_modules", "src/types/graphql.ts"],
   },
-  ...baseConfig,
   {
     rules: {
       '@typescript-eslint/interface-name-prefix': 'off',
