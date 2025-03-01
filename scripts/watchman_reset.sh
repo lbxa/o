b8dca49dabfd694b2d6f2e5bd4376786cfae12a3
lbxa <lucas.brsa@hotmail.com>
2025-03-01 20:38:41 +1100

    chore: Update dependencies and project configurations
    - Upgrade various dependencies across multiple packages
    - Update package names and import paths
    - Add new Graphile API project
    - Modify workspace configuration
    - Update VSCode settings

--- scripts/watchman_reset.sh
+++ scripts/watchman_reset.sh
@@ -2,6 +2,6 @@
 
 # run this at the monorepo root
 # watchman shutdown-server
-watchman watch-del /Users/lchubarbos001/u/o
-watchman watch-project /Users/lchubarbos001/u/o
+watchman watch-del $(pwd)
+watchman watch-project $(pwd)
 watchman
