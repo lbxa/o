4f83072fb1eeb848a966acf137db4ae907a72773
lbxa <lucas.brsa@hotmail.com>
2025-03-02 11:27:44 +1100

    Added error boundary to prevent app crashing

--- apps/onex-mobile/src/app/_layout.tsx
+++ apps/onex-mobile/src/app/_layout.tsx
@@ -11,32 +11,22 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { verifyInstallation } from "nativewind";
 import { useEffect } from "react";
+import { Text, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { loadQuery, RelayEnvironmentProvider } from "react-relay";
 
-import { useRelayEnvironment } from "@/relay";
+import { ErrorBoundary, useRelayEnvironment } from "@/relay";
 import { APP_ROOT_QUERY } from "@/root";
 
 import { useOTheme } from "../utils/useOTheme";
 
 void SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+function AppContent() {
   const { isDark } = useOTheme();
-
-  if (__DEV__) {
-    verifyInstallation();
-  }
-
   const { environment } = useRelayEnvironment();
 
-  /**
-   * The splash screen is the best opportunity
-   * to load as much content as possible to keep
-   * all user journeys snappy and smooth. All subsequent
-   * loading will be cached by relay.
-   */
   useEffect(() => {
     const appRoot = loadQuery(environment, APP_ROOT_QUERY, {});
     void SplashScreen.hideAsync();
@@ -69,5 +59,27 @@ export default function RootLayout() {
         </ThemeProvider>
       </GestureHandlerRootView>
     </RelayEnvironmentProvider>
+  );
+}
+
+export default function RootLayout() {
+  if (__DEV__) {
+    verifyInstallation();
+  }
+
+  // TODO mock up a proper error boundary
+  return (
+    <ErrorBoundary
+      fallback={
+        <View className="text-ivory bg-indigo flex-1 items-center justify-center">
+          <Text className="text-ivory text-6xl font-bold">Uh oh!</Text>
+          <Text className="text-ivory">
+            We've encountered an error. Please try again later.
+          </Text>
+        </View>
+      }
+    >
+      <AppContent />
+    </ErrorBoundary>
   );
 }
--- apps/onex-mobile/src/relay/index.ts
+++ apps/onex-mobile/src/relay/index.ts
@@ -1,2 +1,3 @@
+export * from "./ErrorBoundary";
 export * from "./hooks";
 export * from "./useRelayEnvironment";
