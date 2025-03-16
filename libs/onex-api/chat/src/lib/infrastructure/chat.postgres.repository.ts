84072280cc48fa36b0a6da2ac912eafe3e9c314d
lbxa <lucas.brsa@hotmail.com>
2025-03-16 21:45:14 +1100

    feat: humble beginnings of the new chat interface

---
+++ libs/onex-api/chat/src/lib/infrastructure/chat.postgres.repository.ts
@@ -0,0 +1,5 @@
+import { ChatRepository } from "./chat.repository";
+
+interface ChatPostgresRepository extends ChatRepository {
+
+}
---
+++ libs/onex-api/chat/src/lib/infrastructure/chat.repository.ts
@@ -0,0 +1,5 @@
+interface Repository {}
+
+export interface ChatRepository extends Repository {
+
+}
